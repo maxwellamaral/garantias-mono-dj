1600e431267780e5ee4959d59ff8bccf9d6c61f7
Maxwell Anderson <maxwell.amaral@ifpb.edu.br>
2023-04-11 12:06:20 -0300

    🆕Adicionado primeiro caso de testes

---
+++ functional_tests/tests.py
@@ -0,0 +1,18 @@
+# Max acessa a página de cadastro de produtos.
+
+# Ele verifica que o título da página na aba do navegador e
+# na própria página é "Cadastro de produtos".
+
+# Ele verifica que existe um campo para o nome do produto, 
+# data de compra, data de vencimento da garantia, segmento do produto,
+# marca e modelo do produto, local de compra, valor pago pelo produto, 
+# valor da garantia e modo de compra (online ou físico).
+
+# Ele verifica que existe um botão para cadastrar o produto. 
+
+# Ele preenche os campos com os dados do produto que ele deseja cadastrar e 
+# clica no botão para cadastrar o produto. 
+
+# Ele verifica que a página de cadastro de produtos é exibida novamente, 
+# e que existe uma mensagem informando que o produto foi 
+# cadastrado com sucesso.

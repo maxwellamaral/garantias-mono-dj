add600d85211a4de3471a68d11a4edc146a6e302
Maxwell Anderson <maxwell.amaral@ifpb.edu.br>
2023-04-11 10:21:04 -0300

    🆕Criado arquivo de inicialização no PowerShell

--- active_wenv.ps1
+++ active_wenv.ps1
@@ -1,6 +1,2 @@
-
-# SECRET_KEY gerado em https://djecrety.ir/
-$env:DJANGO_SECRET_KEY = 'ia0o-8x75=29v*oxbr=07rv1w_3+^_8ol8o-m8^w8%o@r!4h5u'
-
 Write-Output Ativando ambiente virtual
 ..\..\..\env\wenv\Scripts\activate.ps1
